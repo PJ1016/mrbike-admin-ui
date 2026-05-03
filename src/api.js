22dfb5cfc805684af6911c18c82c32912c7c8c20
mr-bike-doctor <citygaragepvtltd@gmail.com>
2026-05-03 19:12:52 +0530

    feat: initialize API service layer with axios and sweetalert2 integration for admin module

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import Swal from "sweetalert2";
 
+axios.defaults.withCredentials = true;
+
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
   "https://api.mrbikedoctor.cloud/bikedoctor";
